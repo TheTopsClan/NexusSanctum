7044ae5a0af3cbe6c78e5400fa6e9d5d33e59e44
TheTopsClan <topsclangtag@gmail.com>
2025-07-20 20:18:47 +0100

    Update HomeScript.js

--- HomeScript.js
+++ HomeScript.js
@@ -1,31 +1,30 @@
-const LanguageSelect = document.getElementbyID("LanguageSelect");
-const title = document.getElementbyID("Title");
-const StudioDescription = document.getElementbyID("StudioDescription");
-const NavOurGames = document.getElementbyID("NavOurGames");
-const Discord = document.getElementbyID("Discord");
-const English = LanguageSelect.value === "English";
-const Spanish = LanguageSelect.value === "Español";
-const French = LanguageSelect.Value === "Français";
-
 function Change() {
-  if (English) {
-    title.InnerHTML="Nexus Develepor Team";
-    StudioDescription.InnerHTML="Nexus Sanctum developer team is a group of people who make games just for you (yes you). This team has a lot of things for you. Check Theese Out!";
-    NavOurGames.InnerHTML="Our Games";
-    Discord.InnerHTML="Join Our Discord!";
+  const LanguageSelect = document.getElementById("LanguageSelect");
+  const title = document.getElementById("Title");
+  const StudioDescription = document.getElementById("StudioDescription");
+  const NavOurGames = document.getElementById("NavOurGames");
+  const Discord = document.getElementById("Discord");
+
+  const selectedLang = LanguageSelect.value;
+
+  if (selectedLang === "English") {
+    title.innerHTML = "Nexus Developer Team";
+    StudioDescription.innerHTML = "Nexus Sanctum developer team is a group of people who make games just for you (yes you). This team has a lot of things for you. Check These Out!";
+    NavOurGames.innerHTML = "Our Games";
+    Discord.innerHTML = "Join Our Discord!";
   }
 
-if (Spanish) {
-    title.InnerHTML="Equipo de desarrollo de Nexus";
-    StudioDescription.InnerHTML="El equipo de desarrolladores de Nexus Sanctum es un grupo de personas que crean juegos solo para ti (sí, para ti). Este equipo tiene mucho que ofrecerte. ¡Échales un vistazo!";
-    NavOurGames.InnerHTML="Nuestros juegos";
-    Discord.InnerHTML="¡Únete a nuestro discord!";
-}
+  if (selectedLang === "Español") {
+    title.innerHTML = "Equipo de desarrollo de Nexus";
+    StudioDescription.innerHTML = "El equipo de desarrolladores de Nexus Sanctum es un grupo de personas que crean juegos solo para ti (sí, para ti). Este equipo tiene mucho que ofrecerte. ¡Échales un vistazo!";
+    NavOurGames.innerHTML = "Nuestros juegos";
+    Discord.innerHTML = "¡Únete a nuestro discord!";
+  }
 
-if (French) {
-    title.InnerHTML="Équipe de développement Nexus";
-    StudioDescription.InnerHTML="L'équipe de développement de Nexus Sanctum est un groupe de personnes qui créent des jeux spécialement pour vous (oui, vous). Cette équipe a plein de choses à vous offrir. Découvrez-les !";
-    NavOurGames.InnerHTML="Nos jeux";
-    Discord.InnerHTML="Rejoignez notre Discord";
-}
+  if (selectedLang === "Français") {
+    title.innerHTML = "Équipe de développement Nexus";
+    StudioDescription.innerHTML = "L'équipe de développement de Nexus Sanctum est un groupe de personnes qui créent des jeux spécialement pour vous (oui, vous). Cette équipe a plein de choses à vous offrir. Découvrez-les !";
+    NavOurGames.innerHTML = "Nos jeux";
+    Discord.innerHTML = "Rejoignez notre Discord";
+  }
 }
