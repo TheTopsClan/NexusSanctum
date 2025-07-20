109fa76cbc46ba9e6b7c724c3bd7ecd6c1df8f68
TheTopsClan <topsclangtag@gmail.com>
2025-07-20 20:21:07 +0100

    Update HomeScript.js

--- HomeScript.js
+++ HomeScript.js
@@ -12,16 +12,12 @@ function Change() {
     StudioDescription.innerHTML = "Nexus Sanctum developer team is a group of people who make games just for you (yes you). This team has a lot of things for you. Check These Out!";
     NavOurGames.innerHTML = "Our Games";
     Discord.innerHTML = "Join Our Discord!";
-  }
-
-  if (selectedLang === "Español") {
+  } else if (selectedLang === "Español") {
     title.innerHTML = "Equipo de desarrollo de Nexus";
     StudioDescription.innerHTML = "El equipo de desarrolladores de Nexus Sanctum es un grupo de personas que crean juegos solo para ti (sí, para ti). Este equipo tiene mucho que ofrecerte. ¡Échales un vistazo!";
     NavOurGames.innerHTML = "Nuestros juegos";
     Discord.innerHTML = "¡Únete a nuestro discord!";
-  }
-
-  if (selectedLang === "Français") {
+  } else if (selectedLang === "Français") {
     title.innerHTML = "Équipe de développement Nexus";
     StudioDescription.innerHTML = "L'équipe de développement de Nexus Sanctum est un groupe de personnes qui créent des jeux spécialement pour vous (oui, vous). Cette équipe a plein de choses à vous offrir. Découvrez-les !";
     NavOurGames.innerHTML = "Nos jeux";
